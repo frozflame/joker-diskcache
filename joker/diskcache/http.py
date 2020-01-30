ebe00199e06078fb6fec2f07b0387a6f838b087b
frozflame <frozflame@outlook.com>
2020-01-30 15:16:18 +0800

    HTTPClient.get_useragent

--- joker/diskcache/http.py
+++ joker/diskcache/http.py
@@ -63,9 +63,22 @@ class DummyCache(object):
 
 
 class HTTPClient(object):
+    useragents = (
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) '
+        'Gecko/20100101 Firefox/64.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.37 '
+        '(KHTML, like Gecko) Chrome/39.0.2171.88 Safari/537.37',
+    )
+
     def __init__(self, cache):
         self.cache = cache
         self.sess = requests.Session()
+        self.sess.headers['User-Agent'] = self.get_useragent()
+
+    @classmethod
+    def get_useragent(cls):
+        i = int(time.time()) % len(cls.useragents)
+        return cls.useragents[i]
 
     @classmethod
     def cached(cls, dirpath, algo='sha1', prefixlen=4):
